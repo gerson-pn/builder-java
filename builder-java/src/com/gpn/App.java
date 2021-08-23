2874b730af5a8ccb2bbfb7d6bfff0303df3dc8d6
Gerson Penha <gerson.penha@fatec.sp.gov.br>
2021-08-23 15:10:10 -0300

    Inclusão da segunda forma, para criar o padrão builder

--- builder-java/src/com/gpn/App.java
+++ builder-java/src/com/gpn/App.java
@@ -4,20 +4,15 @@ import java.time.LocalDate;
 
 public class App {
 	public static void main(String[] args) {
-		/* Elementos para variar a criação */
-		LocalDate data = LocalDate.now();
-		Cpf cpf = new Cpf("99999999999", data);
+		Cpf cpf = new Cpf("99999999999", LocalDate.now());
 
-		/* Classe que implmenta o padrão builder */
-		ClienteBuilder construtor = new ClienteBuilder();
-
-		Cliente cliente = construtor
+		Cliente cliente = Cliente.builder()
 				.nome("Tony")
 				.sobreNome("Stark")
 				.cpf(cpf)
-				/* outros atributos se existirem... */
 				.build();
-
+		
 		System.out.println(cliente.getNome());
+
 	}
 }
--- builder-java/src/com/gpn/Cliente.java
+++ builder-java/src/com/gpn/Cliente.java
@@ -1,5 +1,6 @@
 package com.gpn;
 
+
 public class Cliente {
 	private String nome;
 	private String sobreNome;
@@ -8,48 +9,84 @@ public class Cliente {
 	private Telefone telefone;
 	private Endereco endereco;
 
-	public Cliente(String nome, String sobreNome, String nomeSocial, Cpf cpf, Telefone telefone, Endereco endereco) {
-		this.nome = nome;
-		this.sobreNome = sobreNome;
-		this.nomeSocial = nomeSocial;
-		this.cpf = cpf;
-		this.telefone = telefone;
-		this.endereco = endereco;
+	private Cliente(ClienteBuilder construtor) {
+		this.nome = construtor.nome;
+		this.sobreNome = construtor.sobreNome;
+		this.nomeSocial = construtor.nomeSocial;
+		this.cpf = construtor.cpf;
+		this.telefone = construtor.telefone;
+		this.endereco = construtor.endereco;
+	}
+	
+	public static ClienteBuilder builder() {
+		return new ClienteBuilder();
+	}
+
+	public static class ClienteBuilder {
+		private String nome;
+		private String sobreNome;
+		private String nomeSocial;
+		private Cpf cpf;
+		private Telefone telefone;
+		private Endereco endereco;
+		
+		public Cliente build() {
+            Cliente cliente =  new Cliente(this);
+            return cliente;
+        }
+		
+		public ClienteBuilder nome(String nome) {
+			this.nome = nome;
+			return this;
+		}
+		public ClienteBuilder sobreNome(String sobreNome) {
+			this.sobreNome = sobreNome;
+			return this;
+		}
+		public ClienteBuilder nomeSocial(String nomeSocial) {
+			this.nomeSocial = nomeSocial;
+			return this;
+		}
+		public ClienteBuilder cpf(Cpf cpf) {
+			this.cpf = cpf;
+			return this;
+		}
+		public ClienteBuilder telefone(Telefone telefone) {
+			this.telefone = telefone;
+			return this;
+		}
+		public ClienteBuilder endereco(Endereco endereco) {
+			this.endereco = endereco;
+			return this;
+		}
 	}
 
 	public String getNomeSocial() {
 		return nomeSocial;
 	}
-
 	public void setNomeSocial(String nomeSocial) {
 		this.nomeSocial = nomeSocial;
 	}
-
 	public Telefone getTelefone() {
 		return telefone;
 	}
-
 	public void setTelefone(Telefone telefone) {
 		this.telefone = telefone;
 	}
-
 	public Endereco getEndereco() {
 		return endereco;
 	}
-
 	public void setEndereco(Endereco endereco) {
 		this.endereco = endereco;
 	}
-
 	public String getNome() {
 		return nome;
 	}
-
 	public String getSobreNome() {
 		return sobreNome;
 	}
-
 	public Cpf getCpf() {
 		return cpf;
 	}
+	
 }
